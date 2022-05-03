e21efc19c982b14d475c87a143b12573fe174e90
MarkMills <98571190+MarkMills@users.noreply.github.com>
2022-05-02 20:09:18 -0600

    Update Prime-Num-While-Do.java

--- Prime-Num-While-Do.java
+++ Prime-Num-While-Do.java
@@ -1,1 +1,25 @@
+class Main {
+    public static void main(String[] args) 
+    {
+    System.out.println("Prime numbers");
+    
+    int i = 2;
+    int count = 0;
 
+    do {
+        if (numberIsPrime(i)) {
+            System.out.println(i);
+            //i++;
+            count++;
+        } i++;
+    } while (count < 25);
+    }
+        public static boolean numberIsPrime(int p) {
+            boolean returnValue = true;
+            for (int x = 2; x <= (p / 2); x++) {
+                if (p % x == 0)
+                    returnValue = false;
+            }    
+        return returnValue;
+        }
+}
